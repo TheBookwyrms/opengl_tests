55fab238689ee80a17133794f62256909b1dbb1c
TheBookwyrms <blackdragon6493@gmail.com>
2024-12-30 13:10:06 -0300

    start on panning, angling

--- opengl_tests/new_test_1/window.py
+++ opengl_tests/new_test_1/window.py
@@ -15,13 +15,19 @@ class window_test_with_openGL:
     def __init__(self):
         self.angle_x, self.angle_y, self.angle_z = 0, 45, 0 # degrees?
         self.pan_x, self.pan_y, self.pan_z = 0, 0, 0
+        self.last_x, self.last_y = 0, 0
         self.zoom = 10
+        self.pan_sensitivity = 0.001
+        self.angle_sensitivity = 0.01
         
         self.width, self.height = 600, 500
         self.aspect_ratio = self.width/self.height
 
+        self.panning, self.angling = False, False
+
     
     def update_camera(self):
+
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         glOrtho(
@@ -49,21 +55,50 @@ class window_test_with_openGL:
         window = glfw.create_window(self.width, self.height, window_name, None, None)
         glfw.make_context_current(window)
         glfw.get_framebuffer_size(window)
-        #self.cursor_key_mouse_callbacks(window)
+        self.cursor_key_mouse_callbacks(window)
 
         return window
 
     def cursor_key_mouse_callbacks(self, window):
         glfw.set_key_callback(window, self.key_callbacks)
         glfw.set_mouse_button_callback(window, self.mouse_callbacks)
+        glfw.set_cursor_pos_callback(window, self.cursor_pos_callbacks)
     
-    def mouse_callbacks(self, window, mouse_x, mouse_y):
-        pass
+    def cursor_pos_callbacks(self, window, xpos, ypos):
+        if self.panning:
+            dx = xpos - self.last_x
+            dy = ypos - self.last_y
+            self.pan_x += dx * self.pan_sensitivity * self.zoom
+            self.pan_y -= dy * self.pan_sensitivity * self.zoom
+
+            self.last_x, self.last_y = xpos, ypos
+
+        if self.angling:
+            dx = xpos - self.last_x
+            dy = ypos - self.last_y
+            self.angle_x += dy * self.angle_sensitivity * self.zoom
+            self.angle_y += dx * self.angle_sensitivity * self.zoom
+
+            self.last_x, self.last_y = xpos, ypos
     
-    def key_callbacks(self, window):
-        escape = glfw.get_key(window, glfw.KEY_ESCAPE)
-        if escape == glfw.PRESS:
-            glfw.terminate()
+    def mouse_callbacks(self, window, button, action, mods):
+        if action == glfw.PRESS:
+            if button == glfw.MOUSE_BUTTON_LEFT:
+                self.panning = True
+            elif button == GLFW_MOUSE_BUTTON_RIGHT:
+                self.angling = True
+        if action == glfw.RELEASE:
+            if button == glfw.MOUSE_BUTTON_LEFT:
+                self.panning = False
+            elif button == GLFW_MOUSE_BUTTON_RIGHT:
+                self.angling = False
+    
+    def key_callbacks(self, window, key, scancode, action, mods):
+        if action == glfw.PRESS:
+            if key == glfw.KEY_ESCAPE:
+                glfw.terminate()
+                self.done = True
+
 
     def main(self):
         if not glfw.init():
@@ -76,16 +111,17 @@ class window_test_with_openGL:
         #test = A()
         test2 = sphere(radius=3)
 
+        self.done = False
 
-        while not glfw.window_should_close(window):
+        while not self.done:
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
             self.update_camera()
 
-            break_loop = check_keys(window)
-            if break_loop:
-                break
+            #break_loop = check_keys(window)
+            #if break_loop:
+            #    break
 
             test2.draw()
             #test.draw()
